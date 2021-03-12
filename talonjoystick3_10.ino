9e9847cf5b10cc927f160c2635170c902fbad2ba
cnbrown99 <78976645+cnbrown99@users.noreply.github.com>
2021-03-12 11:05:34 -0600

    Update talonjoystick3_10.ino

--- talonjoystick3_10.ino
+++ talonjoystick3_10.ino
@@ -3,11 +3,12 @@
 //User configuration:
 int percent = 0;  //between -100 and 100, indicates how fast the motor 
       //will be moving when the arduino boots
-int pins[] = {5}; //the signal output pins (as many as you'd like)
+int pins[] = {5,9}; //the signal output pins (as many as you'd like)
 
 
 const int arraySize = sizeof(pins)/sizeof(int);
-Servo controllers[arraySize];
+Servo controllers;
+
 
 //Joystick Pins
     int x_key = A0;                                               
@@ -16,6 +17,7 @@ Servo controllers[arraySize];
     int y_pos;
     //Motor Pins
     int IN = 5;      //Enable pin for second motor
+    int IN = 5;
     //Initializing variables to store data
     int motor_speed;
 
@@ -24,13 +26,14 @@ void setup ( ) {
   Serial.begin(9600);
  // Serial.println("you called, my guy?\n");
   for (int i=0; i<arraySize; i++)
-    controllers[i].attach(pins[i]); //associate the object to a pin
+    controllers.attach(9); //associate the object to a pin
   delay(1000);
   
     pinMode(IN, OUTPUT);
       //Initializng the joystick pins as input
       pinMode (x_key, INPUT) ;                     
       pinMode (y_key, INPUT) ;
+
   
  // Serial.println("type in a percent, and I will output your PWM.\n");
   
@@ -40,13 +43,12 @@ void setup ( ) {
 void loop() {
         Serial.println(analogRead(x_key));
         x_pos = analogRead (x_key) ;
-        x_pos = digitalRead (x_key) ;
   
   
-  int PWMvalue = percent * 5 + 1500; //scale up to 1000-2000
+ int PWMvalue = (x_pos -512)*.75 + 1500; //scale up to 1000-2000
 
-  for (int i=0; i<arraySize; i++)
-    controllers[i].writeMicroseconds(PWMvalue);
+Serial.println(PWMvalue);
+
 
 //  if (Serial.available() > 1) {
 //    long proposedValue = Serial.parseInt();
@@ -57,20 +59,17 @@ void loop() {
 //      percent = proposedValue;
 
       
-    if (x_pos < 400){     //Rotating the left motor in clockwise direction
-       percent=30;
-       analogWrite(IN, 127);
-       digitalWrite(IN, HIGH);
+    if(x_pos < 450){     //Rotating the left motor in clockwise direction
+      
+       controllers.writeMicroseconds(PWMvalue);
       }
-    else if (x_pos > 400 && x_pos <600){  //Motors will not move when the joystick will be at center
-        percent=0;
-        digitalWrite(IN, LOW);
+  else if(x_pos > 450 && x_pos <550){  //Motors will not move when the joystick will be at center
+        
+        controllers.writeMicroseconds(PWMvalue);
       }
       
-    else if (x_pos > 600){    //Rotating the left motor in anticlockwise direction
-        percent=60;
-        analogWrite(IN,255);
-        digitalWrite(IN, HIGH);
+   else if(x_pos > 550){    //Rotating the left motor in anticlockwise direction
+   
+        controllers.writeMicroseconds(PWMvalue);
         }
     }
-  
